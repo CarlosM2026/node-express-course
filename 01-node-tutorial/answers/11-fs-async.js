e2193019151c822f39b535a4a977fd034456642f
CarlosM2026 <carlosmoncada2022@gmail.com>
2024-06-18 21:14:17 -0400

    answers for lesson 1

---
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -0,0 +1,56 @@
+// const { readFile, writeFile } = require('fs')
+
+// console.log('start')
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   const first = result
+//   readFile('./content/second.txt', 'utf8', (err, result) => {
+//     if (err) {
+//       console.log(err)
+//       return
+//     }
+//     const second = result
+//     writeFile(
+//       './content/result-async.txt',
+//       `Here is the result : ${first}, ${second}`,
+//       (err, result) => {
+//         if (err) {
+//           console.log(err)
+//           return
+//         }
+//         console.log('done with this task')
+//       }
+//     )
+//   })
+// })
+// console.log('starting next task')
+
+
+
+const { writeFile } = require("fs");
+console.log("at start");
+writeFile("./temporary/fileB.txt", "This is line 1\n", (err, result) => {
+  console.log("at point 1");
+  if (err) {
+    console.log("This error happened: ", err);
+  } else {
+    // here you write your next line
+        writeFile("./temporary/fileB.txt", "This is line 2\n", { flag: 'a' }, (err, result) => {
+            console.log("at point 2");
+            if (err) {
+                console.log("This error happened: ", err);
+            } else {
+                writeFile("./temporary/fileB.txt", "This is line 3\n", { flag: 'a' }, (err, result) => {
+                    console.log("at point 3");
+                    if (err) {
+                        console.log("This error happened: ", err);
+            }})
+
+                }
+        })
+    }
+});
+console.log("at end");
