b1747b338b56a2ddd30f6c0680b97c1121f30119
CarlosM2026 <carlosmoncada2022@gmail.com>
2024-08-19 20:28:53 -0400

    week4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,51 @@
 const express = require('express')
 const app = express() 
 
-const { products } = require("./data");
+let { products, people } = require("./data");
 
+const peopleRouter = require("./routes/people") 
 
+const logger = (req, res, next) => {
+    const method = req.method
+    const url = req.url
+    const time = new Date()
+    
+    console.log(method, url, time)
 
+    next() 
+}
+
+// const newEntry = (req, res, next) => {
+//     people = [...people, name]
+//     next() 
+//}
 app.use(express.static("./public"))
 
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+app.use('/api/v1/people',peopleRouter)
+app.use(logger)
+
+//app.use('/api/v1/people', newEntry)
+
+
+// app.get('/api/v1/people', (req, res) => {
+//     res.json({peopleData: people})
+// })
+
+
+// app.post('/api/v1/people', (req, res) => {
+//     const {name} = req.body
+
+//     if (!name) {
+//         res.status(400).json({ success: false, message: "Please provide a name" });
+//     }
+
+//     people.push({ id: people.length + 1, name: req.body.name });
+//     res.status(201).json({ success: true, name: req.body.name });
+
+// })
 
 
 app.get('/api/v1/test',(req,res) => {
@@ -55,6 +94,10 @@ app.get('/api/v1/query', (req,res) => {
     }
 
     res.status(200).json(sortedProducts)
+})
+
+app.get('/', (req, res) => {
+    res.status(200).json({home: "Home"})
 })
 
 
